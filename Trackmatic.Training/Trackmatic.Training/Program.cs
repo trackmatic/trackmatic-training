ecc570201c8ae913e203f26ec200534df600ea3a
ross <rossj@trackmatic.co.za>
2015-08-29 11:22:28 +0200

    Added additional message

--- Trackmatic.Training/Trackmatic.Training/Program.cs
+++ Trackmatic.Training/Trackmatic.Training/Program.cs
@@ -11,6 +11,8 @@ namespace Trackmatic.Training
         {
             Console.WriteLine("Hell World!");
 
+            Console.WriteLine("This is a training app to demonstrate development workflow");
+
             Console.ReadLine();
         }
     }
