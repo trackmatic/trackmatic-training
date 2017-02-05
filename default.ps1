c72a236047d060cd688d607eb5c32586868da77f
RossJayJones <rossj@trackmatic.co.za>
2017-02-05 09:19:46 +0200

    update build script

--- default.ps1
+++ default.ps1
@@ -2,11 +2,14 @@ Framework "4.5.2x64"
 
 properties {
 	$base_dir = $psake.build_script_dir
+    $build_number = $psake.build_number
 }
 
 Task default -depends Clean, Build, Test
 
 Task Build {
+    write-host "Building"
+    write-host $build_number
 }
 
 Task Test {
